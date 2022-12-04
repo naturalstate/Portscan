f7eb14a3c91e2f2ae8a77aac0b016c9073e003c4
naturalstate <erdenham@gmail.com>
2022-12-04 13:25:28 -0600

    First test

---
+++ 1portscan.py
@@ -0,0 +1,53 @@
+from scapy.all import *
+import ipaddress
+
+
+print(r"""\                                                        
+   ________________  ___.__. ______ ____ _____    ____  
+  / ___\_  __ \__  \<   |  |/  ___// ___\\__  \  /    \ 
+ / /_/  >  | \// __ \\___  |\___ \\  \___ / __ \|   |  \
+ \___  /|__|  (____  / ____/____  >\___  >____  /___|  /
+/_____/            \/\/         \/     \/     \/     \/
+
+                     SECURITY SCANNER
+
+""")
+
+ports = [25,80,53,443,445,8080,8443]
+#ports = int(input("Enter ports to scan: "))
+
+
+#how to save revisions in code and github. Save different versions
+#How to automatically find local infor like Gateway, etc, WIFI passwords
+
+def scanSYN(host):
+    ans,unans = sr(
+        IP(dst=host)/
+        TCP(sport=33333,dport=ports,flags="S")
+        ,timeout=2,verbose=0)
+    print("Open ports for host %s" % host)
+    for (s,r,) in ans:
+        if s[TCP].dport == r[TCP].sport and r[TCP].flags=="SA":
+            print(s[TCP].dport)
+    
+def DNSScan(host):
+    ans,unans = sr(
+        IP(dst=host)/
+        UDP(dport=53)/
+        DNS(rd=1,qd=DNSQR(qname="www.google.com"))
+        ,timeout=2,verbose=0)
+    if ans and ans[UDP]:
+        print("DNS query for %s"%host)
+
+host = input("Enter the host to scan: ")
+try:
+    ipaddress.ip_address(host)
+except:
+    print("Invalid host")
+    exit(-1)
+
+scanSYN(host)
+DNSScan(host)
+            
+
+    
---
+++ readme.md
@@ -0,0 +1,1 @@
+A simple SYN and DNS portscanner that uses the dnspython library
